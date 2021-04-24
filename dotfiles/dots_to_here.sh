29339e01666654a696a7d38d43f3279ca3d1a923
Laura <laura.viglioni@loft.com.br>
2021-04-24 16:57:53 -0300

    feat: old dotfiles

---
+++ dotfiles/dots_to_here.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ~/.emacs ./emacs
+cp ~/.bashrc ./shell/bashrc
+cp ~/.i3/config ./i3/i3config
+cp ~/.i3/i3status_top.conf ./i3/i3status_top.conf
+cp ~/.zshrc ./shell/zshrc
+cp ~/.yabairc ./yabai/yabairc
+cp ~/.skhdrc ./yabai/skhdrc
